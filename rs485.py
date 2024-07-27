a1e9815d3d661657ca3af57da101f593d5d3e4bd
Zen <z@pyl.onl>
2024-07-26 21:12:36 -0500

    improve poll frequency
    Signed-off-by: Zen <z@pyl.onl>

--- rs485.py
+++ rs485.py
@@ -7,9 +7,10 @@ class RS485:
     def __init__(self, tx_pin, rx_pin, de_pin, uart=0,
                  baudrate=9600, data_bits=8, parity=None, stop_bits=1,
                  timeout_char=2, poll_interval=100, tx_delay=0,
-                 debug=False):
+                 driver_delay=0.5, debug=False):
         self.debug = debug
         self.baudrate = baudrate
+        self.driver_delay = driver_delay * timeout_char  # Number of char times to wait after sending data
         self.tx_delay = int(tx_delay)  # delay after sending data, in us
         if tx_delay > 0:
             timeout = int(tx_delay / 1000 + timeout_char)  # timeout for uart read, in ms
@@ -20,8 +21,7 @@ class RS485:
         self.log(f"Initialized UART: {self.uart}")
         self.de = Pin(de_pin, mode=Pin.OUT)
 
-        self.poll_interval = int(poll_interval)
-        print("Poll interval: ", self.poll_interval)
+        self.poll_interval = int(poll_interval)  # 0 to poll continuously
         self.messages = []
 
         self.run = Event()
@@ -37,7 +37,8 @@ class RS485:
         while True:
             await self.run.wait()
             await self.recv()
-            await sleep_ms(self.poll_interval)
+            if self.poll_interval:
+                await sleep_ms(self.poll_interval)
 
     async def recv(self):
         async with self.dev_lock:
@@ -50,18 +51,15 @@ class RS485:
             self.messages.append(data)
 
     def _send(self, data):
-        """
-        Send wrapper.
-        doesn't return the DE pin to receive mode, as timing is handled by async
-        """
         self.de.on()
         self.log(f"Sending data: {data.hex()}")
         self.uart.write(data)
         self.uart.flush()
+        sleep_us(int(self.driver_delay * 1000))  # Wait for the rs485 driver to complete the transmission
+        self.de.off()  # Only disable DE after the uart write is complete
 
     async def send(self, data):
         async with self.dev_lock:
             self._send(data)
-            sleep_us(self.tx_delay)
-            self.de.off()
+            sleep_us(self.tx_delay)  # Sleep between consecutive transmissions
 
